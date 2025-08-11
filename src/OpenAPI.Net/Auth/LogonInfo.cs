5c7791ae74a10a662d2fefa211729424dae94fdd
leung0514 <leung0514@gmail.com>
2025-08-12 00:28:33 +0800

    Create LogonInfo.cs

---
+++ src/OpenAPI.Net/Auth/LogonInfo.cs
@@ -0,0 +1,12 @@
+﻿using OpenAPI.Net.Helpers;
+
+namespace OpenAPI.Net.Auth;
+
+public sealed class LogonInfo
+{
+    public Mode Mode { get; set; }
+    public string ClientId { get; set; } = string.Empty;
+    public string ClientSecret { get; set; } = string.Empty;
+    public long AccountId { get; set; }
+    public string AccessToken { get; set; } = string.Empty;
+}
